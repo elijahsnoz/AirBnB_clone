ea23d57142c2ad8deb89e3cdf680b8a373ecbd81
azzaomer <azzaomer70@gmail.com>
2024-05-14 07:45:47 -0700

    task4

--- models/base_model.py
+++ models/base_model.py
@@ -3,14 +3,26 @@
 import uuid
 from datetime import datetime
 
+
 class BaseModel:
     """Defines all common attributes/methods for other classes"""
 
-    def __init__(self):
+    def __init__(self, *args, **kwargs):
         """Initialize instance attributes"""
-        self.id = str(uuid.uuid4())
-        self.created_at = datetime.now()
-        self.updated_at = datetime.now()
+        DATE_TIME = '%Y-%m-%dT%H:%M:%S.%f'
+        if kwargs:
+            for key, value in kwargs.items():
+                if key != '__class__':
+                    if key in ['created_at', 'updated_at']:
+                        self.__dict__[key]=datetime.strptime(value, DATE_TIME)
+                    elif key[0]=="id":
+                        self.__dict__[key]=str(value)
+                    else:
+                        self.__dict__[key]=value
+        else:
+            self.id = str(uuid.uuid4())
+            self.created_at=datetime.now()
+            self.updated_at=datetime.now()
 
     def __str__(self):
         """Return string representation of the instance"""
