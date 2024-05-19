ae86d8de985b47cb22cab69199d0a4a0cfdddcab
elijahsnoz <elijahsnoz@gmail.com>
2024-05-19 11:57:52 +0100

    (fix) bug

--- models/base_model.py
+++ models/base_model.py
@@ -15,8 +15,7 @@ class BaseModel:
             for key, value in kwargs.items():
                 if key != '__class__':
                     if key in ['created_at', 'updated_at']:
-                        self.__dict__[key] =
-                        datetime.strptime(value, DATE_TIME)
+                        self.__dict__[key] = datetime.strptime(value, DATE_TIME)
                     else:
                         self.__dict__[key] = value
         else:
