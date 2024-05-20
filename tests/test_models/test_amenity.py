9239e7c488f0337663daf3d7bd2834e494e50e8b
elijahsnoz <elijahsnoz@gmail.com>
2024-05-20 16:02:41 +0100

    Update from dictionary

---
+++ tests/test_models/test_amenity.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+from models.base_model import BaseModel
+
+
+class Amenity(BaseModel):
+    """Custom amenity class
+
+    Attributes:
+        name(str): amenity name
+
+    """
+    name = ""
