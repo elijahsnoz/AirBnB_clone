ff3639484d594b1bcd9c555fd6740e32c88c6ed3
azzaomer <azzaomer70@gmail.com>
2024-05-21 04:00:11 -0700

    base model

--- models/base_model.py
+++ models/base_model.py
@@ -46,7 +46,7 @@ class BaseModel:
 
     def to_dict(self):
         """Return a dictionary containing all keys/values of __dict__"""
-        instance_dict =self.__dict__.copy()""" {**self.__dict__}"""
+        instance_dict = {**self.__dict__}
         instance_dict["__class__"] = type(self).__name__
         instance_dict["created_at"] = instance_dict["created_at"].isoformat()
         instance_dict['updated_at'] = instance_dict["updated_at"].isoformat()
