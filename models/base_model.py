5fc896e98f3b31fbe3a7d739209a3bbc3d074434
azzaomer <azzaomer70@gmail.com>
2024-05-21 04:34:05 -0700

    base model

--- models/base_model.py
+++ models/base_model.py
@@ -17,15 +17,15 @@ class BaseModel:
             - *args: list of arguments
             - **kwargs: dict of key-values arguments
         """
-
+        DateTime = "%Y-%m-%dT%H:%M:%S.%f"
         if kwargs is not None and kwargs != {}:
             for key in kwargs:
                 if key == "created_at":
                     self.__dict__["created_at"] = datetime.strptime(
-                        kwargs["created_at"], "%Y-%m-%dT%H:%M:%S.%f")
+                        kwargs["created_at"], DateTime)
                 elif key == "updated_at":
                     self.__dict__["updated_at"] = datetime.strptime(
-                        kwargs["updated_at"], "%Y-%m-%dT%H:%M:%S.%f")
+                        kwargs["updated_at"], DateTime)
                 else:
                     self.__dict__[key] = kwargs[key]
         else:
@@ -37,8 +37,8 @@ class BaseModel:
     def __str__(self):
         """Returns official string representation"""
 
-        return "[{}] ({}) {}".\
-            format(type(self).__name__, self.id, self.__dict__)
+        return "[{}] ({}) {}".format(
+                type(self).__name__, self.id, self.__dict__)
 
     def save(self):
         """updates the public instance attribute updated_at"""
@@ -49,8 +49,8 @@ class BaseModel:
     def to_dict(self):
         """returns a dictionary containing all keys/values of __dict__"""
 
-        my_dict = self.__dict__.copy()
-        my_dict["__class__"] = type(self).__name__
-        my_dict["created_at"] = my_dict["created_at"].isoformat()
-        my_dict["updated_at"] = my_dict["updated_at"].isoformat()
-        return my_dict
+        instance_dict = self.__dict__.copy()
+        instance_dict["__class__"] = type(self).__name__
+        instance_dict["created_at"] = instance_dict["created_at"].isoformat()
+        instance_dict["updated_at"] = instance_dict["updated_at"].isoformat()
+        return instance_dict
