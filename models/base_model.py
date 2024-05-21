4bdd22aaa4199b8d836ed8dae6ef44ca9be240bf
azzaomer <azzaomer70@gmail.com>
2024-05-21 04:25:13 -0700

    base model

--- models/base_model.py
+++ models/base_model.py
@@ -12,24 +12,21 @@ class BaseModel:
     def __init__(self, *args, **kwargs):
         """Initialize instance attributes"""
         from models import storage  # Delayed import to avoid circular import
-        DATE_TIME = '%Y-%m-%dT%H:%M:%S.%f'
-        if kwargs:
-            for key, value in kwargs.items():
-                if key == '__class__':
-                    continue
-                if key in ['created_at', 'updated_at']:
-                    value = datetime.fromisoformat(value)
-                setattr(self, key, value)
-            return
-            """string_date = datetime.strptime(value, DATE_TIME)
-                    self.__dict__[key] = string_date
+        if kwargs is not None and kwargs != {}:
+            for key in kwargs:
+                if key == "created_at":
+                    self.__dict__["created_at"] = datetime.strptime(
+                        kwargs["created_at"], "%Y-%m-%dT%H:%M:%S.%f")
+                elif key == "updated_at":
+                    self.__dict__["updated_at"] = datetime.strptime(
+                        kwargs["updated_at"], "%Y-%m-%dT%H:%M:%S.%f")
                 else:
-                    self.__dict__[key] = value"""
-
-        self.id = str(uuid.uuid4())
-        self.created_at = datetime.now()
-        self.updated_at = datetime.now()
-        models.storage.new(self)  # Add new instance to storage
+                    self.__dict__[key] = kwargs[key]
+        else:
+            self.id = str(uuid.uuid4())
+            self.created_at = datetime.now()
+            self.updated_at = datetime.now()
+            models.storage.new(self)  # Add new instance to storage
 
     def __str__(self):
         """Return string representation of the instance"""
